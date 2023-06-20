621ac37b2a2191f71982ce039ae8a24305e52403
Cooper <cooper.softdev@gmail.com>
2023-06-19 18:18:00 -0800

    trello requirements forntend rendering

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -13,15 +13,15 @@ class BestBooks extends React.Component {
     this.getAllBooks();
   }
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
-
+  
   // todo: define handler to get all books 
   getAllBooks = async () => {
+    let url = `${process.env.REACT_APP_BOOK_SERVER}/books`
+    console.log('This is the URL', url);
     try {
       // todo: make a call to my server and hit my books endpoint
-
-      let url = `${process.env.BOOK_SERVER}/books`
+      
       let booksFromDB = await axios.get(url);
-      console.log('This is the URL', url);
       console.log('This is books from BD', booksFromDB);
       // todo: save the response from my server to my state
       this.setState({
@@ -37,15 +37,19 @@ class BestBooks extends React.Component {
 
 
   render() {
-
-    /* TODO: render all the books in a Carousel */
-
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+  
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <div>
+            <h3>Books:</h3>
+            <ul>
+              {this.state.books.map(book => (
+                <li key={book._id}>{book.title} by {book.author}</li>
+              ))}
+            </ul>
+          </div>
         ) : (
           <h3>No Books Found :(</h3>
         )}
@@ -53,5 +57,4 @@ class BestBooks extends React.Component {
     )
   }
 }
-
 export default BestBooks;
